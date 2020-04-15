7f0869dda543b4055f9449ebe0cef4ad3cfaca8e
DarNattp <DarNattp@github.com>
2020-04-16 01:34:22 +0700

    comment

--- Draw and ruler.py
+++ Draw and ruler.py
@@ -1,24 +1,20 @@
-def drawLine(tickLength, tickLabel=''):
+def drawLine(tickLength, tickLabel=''): #creat line from receive data of other fuction.
     line = '-' * tickLength
     if tickLabel:
         line += ' ' + tickLabel
     print(line)
 
-def drawInterval(centerLength):
+def drawInterval(centerLength): #define line and sent to drawLine funtion to creat line 
     if centerLength > 0:
         drawInterval(centerLength-1)
         drawLine(centerLength)
         drawInterval(centerLength-1)
 
-def drawRuler(numInches, majorLength):
-    drawLine(majorLength, '0')
+def drawRuler(numInches, majorLength): #main in create ruler : numInches is number main of ruler. 
+    drawLine(majorLength, '0')          #majorLength is range between main number of ruler.
     for i in range(1, numInches+1):
-        drawInterval(majorLength-1)
-        drawLine(majorLength, str(i))
-
-
-
-
+        drawInterval(majorLength-1)     #creat recursive line between main line or number
+        drawLine(majorLength, str(i)) #creat line and number of main line
 
 
 if __name__ == '__main__':
